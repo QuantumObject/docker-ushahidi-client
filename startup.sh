41f5c96119b2effb1e07f6b3c95914d4702a01b9
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-31 11:47:36 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,6 +7,12 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
       #code that need to run only one time ....
+        cd /var/www/platform-client
+        #need to add .env file with the ushahidi-platform web location.BACKEND_URL=http://myapi.server/
+        touch .env
+        echo 
+        gulp build
+        cp /var/www/platform-client/server/rewrite.htaccess /var/www/platform-client/server/www/.htaccess
         
         #needed for fix problem with ubuntu and cron
         update-locale 
