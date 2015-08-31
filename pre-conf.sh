230109793ecc8ebf09dab00fc88c6f8829fd23da
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-31 13:36:23 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -7,3 +7,10 @@ git submodule update --init --recursive
 cd /var/www/platform-client
 npm install -g gulp
 npm install
+
+a2enmod rewrite
+rm -R /var/www/html
+
+#to fix error relate to ip address of container apache2
+echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
+ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
