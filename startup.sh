617cdaa1d92990caadba0abc8d044912c33cd82c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-17 22:45:22 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -15,7 +15,7 @@ else
         echo "APP_LANGUAGES=en" >> .env
         echo "OAUTH_CLIENT_ID=ushahidiui" >> .env
         echo "OAUTH_CLIENT_SECRET=$APP_KEY" >> .env
-        gulp build
+        # gulp build
         cp /var/www/platform-client/server/rewrite.htaccess /var/www/platform-client/server/www/.htaccess
         
         #needed for fix problem with ubuntu and cron
